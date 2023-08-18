e9de11f9bef2d9dad96f4e1de503ae4ecbb93dbc
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-08-18 18:27:38 +0700

    lan1

---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public Rigidbody myrb;
+    public float speed = 10;
+    public float boostspeed = 40;
+    public float currentSpeed;
+    public float jumpForce = 10;
+        private void Start()
+    {
+        currentSpeed = speed;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            currentSpeed = boostspeed;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
+        float XLeftRight = Input.GetAxis("Horizontal");
+        float zForwardbackward = Input.GetAxis("Vertical");
+        transform.position += new Vector3(XLeftRight, 0, zForwardbackward) * currentSpeed * Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space) )
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        myrb.velocity +=new Vector3(0,jumpForce,0);
+    }
+}
