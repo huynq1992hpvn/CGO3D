b60ac0d1fdab662e0d56f39c6d50c3627721fa1f
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-08-18 18:44:00 +0700

    lan1

---
+++ Assets/Scripts/CameraMouse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouse : MonoBehaviour
+{
+    public float mouseSensitivity = 2.0f; 
+    public Transform playerBody; 
+
+    private float xRotation = 0.0f;
+
+    
+}
--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -1,10 +1,11 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
+    private bool isGrounded;
     public Rigidbody myrb;
     public float speed = 10;
     public float boostspeed = 40;
@@ -30,14 +31,22 @@ public class CharacterMove : MonoBehaviour
         float XLeftRight = Input.GetAxis("Horizontal");
         float zForwardbackward = Input.GetAxis("Vertical");
         transform.position += new Vector3(XLeftRight, 0, zForwardbackward) * currentSpeed * Time.deltaTime;
-        if(Input.GetKeyDown(KeyCode.Space) )
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
+            isGrounded = false;
         }
     }
 
     private void Jump()
     {
         myrb.velocity +=new Vector3(0,jumpForce,0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; 
+        }
     }
 }
