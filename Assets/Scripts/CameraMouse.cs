0bdde9920e215bba4979f97c4aa8d42046c299b4
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-08-18 19:16:01 +0700

    lan1

--- Assets/Scripts/CameraMouse.cs
+++ Assets/Scripts/CameraMouse.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraMouse : MonoBehaviour
 {
@@ -9,5 +7,21 @@ public class CameraMouse : MonoBehaviour
 
     private float xRotation = 0.0f;
 
-    
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); 
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); 
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
 }
